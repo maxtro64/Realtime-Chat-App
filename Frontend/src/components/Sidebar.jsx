ba4ec65f1e9a6e7e339ebc9c3564d1baf8da22aa
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 18:45:46 +0530

    Update Sidebar.jsx

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
     console.log("All Users",users);
     console.log("Online Users",filteredUsers);
     getUsers();
-  }, [getUsers,filteredUsers]);
+  }, [getUsers]);
 
   if (isUsersLoading) return <SidebarSkeleton />;
 
