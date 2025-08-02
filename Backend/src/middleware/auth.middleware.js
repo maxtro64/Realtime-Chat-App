baeb5a275d21a2c527f790a8997b716ab963ca55
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 23:48:19 +0530

    Update auth.middleware.js

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -3,7 +3,9 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt;\
+
+    console.log(token)
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No Token Provided" });
