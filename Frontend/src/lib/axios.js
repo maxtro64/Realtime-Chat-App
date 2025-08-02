9d5945bb009a18b4fc4f52d2fd24cd9978a4c8d7
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 20:32:01 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE==="development"?'http://localhost:5001/api':"https://realtime-chatty-app-wv5e.onrender.com/api",
+  baseURL: "https://realtime-chatty-app-wv5e.onrender.com/api",
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
