6817de32a8e6c8de1742339023faf28e05af6462
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 17:43:36 +0530

    Update useAuthStore.js

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -19,6 +19,7 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
+      console.log(res)
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
