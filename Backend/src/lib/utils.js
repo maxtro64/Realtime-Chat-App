2f22c07e3e7998705bad2b58714c2cd0b4fc3078
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 18:29:55 +0530

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -9,6 +9,7 @@ res.cookie('jwt', token, {
   httpOnly: true,
   secure: true, // REQUIRED for HTTPS
   sameSite: 'none', // REQUIRED for cross-site
+   domain: '.vercel.app', 
   maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 });
 
