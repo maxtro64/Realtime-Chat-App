568dfd26b6afbe172df956ad6323fa61e733ec12
Shivam Yadav <shivam2110207@gmail.com>
2025-08-02 23:09:26 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://realtime-chat-app-tan-alpha.vercel.app",
+    origin: "https://realtime-chat-app-tan-alpha.vercel.app",
     credentials: true,
     exposedHeaders: ['set-cookie']
   })
